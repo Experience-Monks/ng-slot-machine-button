9f05e14cd31ecb22c399ed14e01fd42cb13f95f6
Neo <neo.li@jam3.com>
2017-11-13 12:04:32 -0500

    Add a demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { SlotMachineButtonModule } from './slot-machine-button/slot-machine-button.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
+    SlotMachineButtonModule,
     BrowserModule
   ],
   providers: [],

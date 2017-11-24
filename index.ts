0c8f8a2c551f7be3a4304e879177a03e18354ba8
Neo <neo.li@jam3.com>
2017-11-24 13:45:32 -0500

    Add some meta data

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { SlotMachineButtonComponent } from './src/app/slot-machine-button/slot-machine-button.component';
+export { SlotMachineWheelComponent } from './src/app/slot-machine-button/slot-machine-wheel.component';
+export { SlotMachineButtonModule } from './src/app/slot-machine-button/slot-machine-button.module';
